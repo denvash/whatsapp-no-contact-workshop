c6812172e00bd86427e922df1230b030619ad57f
Dennis Vash <dennisvash@fb.com>
2021-07-13 17:31:04 +0300

    init packages

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+// https://prettier.io/docs/en/configuration.html
+
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: false,
+  jsxBracketSameLine: true,
+  arrowParens: 'avoid',
+};
