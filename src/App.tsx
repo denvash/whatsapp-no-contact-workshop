b43acb8ffd9ca327432a40bf9f433135b9f3ac69
Dennis Vash <dennisvash@fb.com>
2021-07-26 10:42:34 +0300

    text area added

--- src/App.tsx
+++ src/App.tsx
@@ -1,80 +1,79 @@
-import {BackgroundDotsPattern} from './components/BackgroundDotsPattern';
-
-function App() {
+export function App() {
   return (
-    <div className="">
-      <div className="bg-white py-16 sm:py-24">
-        <div className="relative sm:py-16">
-          <div aria-hidden="true" className="hidden sm:block">
-            <div className="absolute inset-y-0 left-0 w-1/2 bg-gray-50 rounded-r-3xl" />
-            <BackgroundDotsPattern />
+    <div className="bg-white w-screen h-screen flex justify-center items-center justify-items-center">
+      <div className="w-full max-w-md px-4 sm:max-w-3xl sm:px-6 lg:max-w-7xl lg:px-8">
+        <div className="relative rounded-2xl px-6 py-10 bg-green-500 overflow-hidden shadow-xl sm:px-12 sm:py-20">
+          <div
+            aria-hidden="true"
+            className="absolute inset-0 -mt-72 sm:-mt-32 md:mt-0">
+            <svg
+              className="absolute inset-0 h-full w-full"
+              preserveAspectRatio="xMidYMid slice"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 1463 360">
+              <path
+                className="text-green-300 text-opacity-20"
+                fill="currentColor"
+                d="M-82.673 72l1761.849 472.086-134.327 501.315-1761.85-472.086z"
+              />
+              <path
+                className="text-green-400 text-opacity-40"
+                fill="currentColor"
+                d="M-217.088 544.086L1544.761 72l134.327 501.316-1761.849 472.086z"
+              />
+            </svg>
           </div>
-          <div className="mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:max-w-7xl lg:px-8">
-            <div className="relative rounded-2xl px-6 py-10 bg-green-600 overflow-hidden shadow-xl sm:px-12 sm:py-20">
-              <div
-                aria-hidden="true"
-                className="absolute inset-0 -mt-72 sm:-mt-32 md:mt-0">
-                <svg
-                  className="absolute inset-0 h-full w-full"
-                  preserveAspectRatio="xMidYMid slice"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 1463 360">
-                  <path
-                    className="text-green-500 text-opacity-40"
-                    fill="currentColor"
-                    d="M-82.673 72l1761.849 472.086-134.327 501.315-1761.85-472.086z"
-                  />
-                  <path
-                    className="text-green-700 text-opacity-40"
-                    fill="currentColor"
-                    d="M-217.088 544.086L1544.761 72l134.327 501.316-1761.849 472.086z"
-                  />
-                </svg>
+          <div className="relative">
+            <div className="flex flex-col gap-4">
+              <div className="sm:text-center flex flex-col gap-2">
+                <h2 className="text-3xl font-extrabold text-white tracking-tight sm:text-4xl">
+                  No Contact Whatsapp Message
+                </h2>
+                <p className="mx-auto max-w-2xl text-lg text-green-200">
+                  It's free, just use it
+                </p>
               </div>
-              <div className="relative">
-                <div className="sm:text-center">
-                  <h2 className="text-3xl font-extrabold text-white tracking-tight sm:text-4xl">
-                    No Contact Whatsapp Message
-                  </h2>
-                  <p className="mt-6 mx-auto max-w-2xl text-lg text-green-200">
-                    It's free, just use it
-                  </p>
-                </div>
-                <form
-                  action="#"
-                  className="mt-12 sm:mx-auto sm:max-w-lg sm:flex">
-                  <div className="relative rounded-md shadow-sm">
-                    <div className="absolute inset-y-0 left-0 flex items-center">
-                      <label htmlFor="country" className="sr-only">
-                        Country
-                      </label>
-                      <select
-                        id="country"
-                        name="country"
-                        className="focus:ring-indigo-500 focus:border-indigo-500 h-full py-0 px-3 border-transparent bg-transparent text-gray-500 sm:text-sm rounded-md">
-                        <option>US</option>
-                        <option>CA</option>
-                        <option>EU</option>
-                      </select>
-                    </div>
+              <form
+                action="#"
+                className="sm:mx-auto sm:max-w-lg sm:flex flex-col gap-4">
+                <div className="flex gap-4">
+                  <div className="flex rounded-md shadow-sm flex-grow">
+                    <input
+                      className="inline-flex items-center px-3 rounded-l-md  border-r-0 bg-gray-50 text-gray-500 sm:text-sm w-16"
+                      placeholder="054"
+                    />
                     <input
                       type="text"
-                      name="phone-number"
-                      id="phone-number"
-                      className="pl-20 block w-full border border-transparent rounded-md px-2 py-3 text-base text-gray-900 placeholder-gray-500 shadow-sm focus:outline-none focus:border-transparent focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-green-600"
-                      placeholder="+1 (555) 987-6543"
+                      name="company-website"
+                      id="company-website"
+                      className="flex-1 min-w-0 block w-full px-3 py-2 rounded-none rounded-r-md focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border-gray-300"
+                      placeholder="5554321"
                     />
                   </div>
-                  <div className="mt-4 sm:mt-0 sm:ml-3">
+                  <div>
                     <button
                       type="submit"
-                      className="block w-full rounded-md border bg-opacity-100 border-transparent px-5 py-3 bg-green-500 text-base font-medium text-white shadow hover:bg-green-300 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-green-600 sm:px-10">
+                      className="block w-full rounded-md border bg-opacity-100 border-transparent px-5 py-3 bg-green-400 text-base font-medium text-white shadow hover:bg-green-300 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-green-600 sm:px-10">
                       Send
                     </button>
                   </div>
-                </form>
-              </div>
+                </div>
+                <div className="border border-gray-300 rounded-md px-3 py-2 shadow-sm focus-within:ring-1 focus-within:ring-green-600 focus-within:border-green-600 bg-white">
+                  <label
+                    htmlFor="message"
+                    className="block text-xs font-medium text-gray-900">
+                    Message
+                  </label>
+                  <textarea
+                    rows={5}
+                    name="message"
+                    id="message"
+                    className="block w-full border-0 p-0 text-gray-900 placeholder-gray-500 focus:ring-0 sm:text-sm outline-none"
+                    placeholder="Hello there!"
+                  />
+                </div>
+              </form>
             </div>
           </div>
         </div>
@@ -82,5 +81,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
